0b193760aa189990f8b90ff5a024984de79d9769
Vitalik Buterin <VitalikZhou@gmail.com>
2021-06-05 15:44:57 +0800

    write file -> stdout

--- xlog.go
+++ xlog.go
@@ -3,70 +3,24 @@ package xlog
 import (
 	"fmt"
 	"log"
-	"os"
-	"path"
 )
 
-const (
-	logChanSize = 1 << 16
-)
-
-type xlog struct {
-	c chan string
-	w *log.Logger
+func Debug(msg string, args ...interface{}) {
+	log.Printf("[DEBUG] %s\n", fmt.Sprintf(msg, args...))
 }
 
-// XLog is the log interface
-type XLog interface {
-	Debug(msg string, args ...interface{})
-	Warn(msg string, args ...interface{})
-	Info(msg string, args ...interface{})
-	Error(msg string, args ...interface{})
-	Crit(msg string, args ...interface{})
+func Warn(msg string, args ...interface{}) {
+	log.Printf("[WARN] %s", fmt.Sprintf(msg, args...))
 }
 
-func checklogPath(filename string) {
-	fileinfo, err := os.Stat(filename)
-	if err == nil && fileinfo.IsDir() {
-		panic("log path must be is file")
-	}
-	dir := path.Dir(filename)
-	err = os.MkdirAll(dir, 0777)
-	if err != nil {
-		panic(fmt.Sprintf("create log dir failed, %s", err.Error()))
-	}
+func Info(msg string, args ...interface{}) {
+	log.Printf("[INFO] %s", fmt.Sprintf(msg, args...))
 }
 
-// NewDailyLog create a daily log wrapper
-func NewDailyLog(path string) XLog {
-	checklogPath(path)
-	_log := xlog{w: log.New(NewDailyWriter(path), "", log.LstdFlags), c: make(chan string, logChanSize)}
-	go _log.writer()
-	return &_log
+func Error(msg string, args ...interface{}) {
+	log.Printf("[ERROR] %s", fmt.Sprintf(msg, args...))
 }
 
-func (log *xlog) Debug(msg string, args ...interface{}) {
-	log.c <- fmt.Sprintf("[DEBUG] %s", fmt.Sprintf(msg, args...))
-}
-
-func (log *xlog) Warn(msg string, args ...interface{}) {
-	log.c <- fmt.Sprintf("[WARN] %s", fmt.Sprintf(msg, args...))
-}
-
-func (log *xlog) Info(msg string, args ...interface{}) {
-	log.c <- fmt.Sprintf("[INFO] %s", fmt.Sprintf(msg, args...))
-}
-
-func (log *xlog) Error(msg string, args ...interface{}) {
-	log.c <- fmt.Sprintf("[ERROR] %s", fmt.Sprintf(msg, args...))
-}
-
-func (log *xlog) Crit(msg string, args ...interface{}) {
-	log.c <- fmt.Sprintf("[CRIT] %s", fmt.Sprintf(msg, args...))
-}
-
-func (log *xlog) writer() {
-	for {
-		log.w.Println(<-log.c)
-	}
+func Crit(msg string, args ...interface{}) {
+	log.Printf("[CRIT] %s", fmt.Sprintf(msg, args...))
 }
